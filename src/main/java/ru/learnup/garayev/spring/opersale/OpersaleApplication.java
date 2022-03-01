6a033badb36b50850f0dc142a5ac3b50993d812b
puhnat <gai731@rambler.ru>
2022-03-01 21:25:31 +0300

    fail component

--- src/main/java/ru/learnup/garayev/spring/opersale/OpersaleApplication.java
+++ src/main/java/ru/learnup/garayev/spring/opersale/OpersaleApplication.java
@@ -15,38 +15,39 @@ public class OpersaleApplication {
         final ConfigurableApplicationContext ctx = SpringApplication.run(OpersaleApplication.class, args);
 
         ctx.getBean(OpersaleApplication.class);
-        ctx.getBean(TheatreSeason.class).setName("Февраль 2022");
+        TheatreSeason theatreSeason = ctx.getBean(TheatreSeason.class);
+        theatreSeason.setName("Февраль 2022");
 
         for (int i = 0; i < 3; i++) {
-            ctx.getBean(TheatreSeason.class).addTheatreSeason("Премьера " + i, "[" + i + "] Премьера супер спектакля", 0, 200, LocalDateTime.of(2022, Month.FEBRUARY, 1+i, 18, 0, 0));
+            theatreSeason.addTheatreSeason("Премьера " + i, "[" + i + "] Премьера супер спектакля", 0, 200, LocalDateTime.of(2022, Month.FEBRUARY, 1+i, 18, 0, 0));
         }
 
         // Все премьеры
-        printAll(ctx.getBean(TheatreSeason.class), "");
+        printAll(theatreSeason, "");
 
         // Одна премьера
-        ctx.getBean(TheatreSeason.class).infoAboutPremier(LocalDateTime.of(2022, Month.FEBRUARY, 1, 18, 0, 0));
+        theatreSeason.infoAboutPremier(LocalDateTime.of(2022, Month.FEBRUARY, 1, 18, 0, 0));
         System.out.println();
 
         // Продажа билетов
-        ctx.getBean(TheatreSeason.class).buyTicket(ctx.getBean(TheatreSeason.class).getRealTheatrePremier(LocalDateTime.of(2022, Month.FEBRUARY, 1, 18, 0, 0)), 10);
-        ctx.getBean(TheatreSeason.class).buyTicket(ctx.getBean(TheatreSeason.class).getRealTheatrePremier(LocalDateTime.of(2022, Month.FEBRUARY, 2, 18, 0, 0)), 20);
-        ctx.getBean(TheatreSeason.class).buyTicket(ctx.getBean(TheatreSeason.class).getRealTheatrePremier(LocalDateTime.of(2022, Month.FEBRUARY, 3, 18, 0, 0)), 50);
+        theatreSeason.buyTicket(theatreSeason.getRealTheatrePremier(LocalDateTime.of(2022, Month.FEBRUARY, 1, 18, 0, 0)), 10);
+        theatreSeason.buyTicket(theatreSeason.getRealTheatrePremier(LocalDateTime.of(2022, Month.FEBRUARY, 2, 18, 0, 0)), 20);
+        theatreSeason.buyTicket(theatreSeason.getRealTheatrePremier(LocalDateTime.of(2022, Month.FEBRUARY, 3, 18, 0, 0)), 50);
 
         // Все премьеры
-        printAll(ctx.getBean(TheatreSeason.class), "После продажи билетов");
+        printAll(theatreSeason, "После продажи билетов");
 
         // Возврат билетов
-        ctx.getBean(TheatreSeason.class).returnTicket(ctx.getBean(TheatreSeason.class).getRealTheatrePremier(LocalDateTime.of(2022, Month.FEBRUARY, 2, 18, 0, 0)), 20);
+        theatreSeason.returnTicket(theatreSeason.getRealTheatrePremier(LocalDateTime.of(2022, Month.FEBRUARY, 2, 18, 0, 0)), 20);
 
         // Все премьеры
-        printAll(ctx.getBean(TheatreSeason.class), "После возврата билетов");
+        printAll(theatreSeason, "После возврата билетов");
 
         // Перенос премьеры
-        ctx.getBean(TheatreSeason.class).replaceTheatreSeason(ctx.getBean(TheatreSeason.class).getRealTheatrePremier(LocalDateTime.of(2022, Month.FEBRUARY, 2, 18, 0, 0)), LocalDateTime.of(2022, Month.FEBRUARY, 15, 18, 0, 0));
+        theatreSeason.replaceTheatreSeason(theatreSeason.getRealTheatrePremier(LocalDateTime.of(2022, Month.FEBRUARY, 2, 18, 0, 0)), LocalDateTime.of(2022, Month.FEBRUARY, 15, 18, 0, 0));
 
         // Все премьеры
-        printAll(ctx.getBean(TheatreSeason.class), "После переноса премьеры");
+        printAll(theatreSeason, "После переноса премьеры");
 
     }
 
